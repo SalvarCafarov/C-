0833cdd9ff8dc8815074357d361503eb52ba7377
salvar <salvarcafarov@gmail.com>
2024-03-07 10:50:06 +0400

    task2

---
+++ task2(07.03)/Program.cs
@@ -0,0 +1,33 @@
+﻿//int row = Convert.ToInt32(Console.ReadLine()); 
+
+//for(int i = 1; i < row; i++)
+//{
+//    string star = "*";
+//    for(int j = 0; j < i; j++)
+//    {
+
+//    Console.Write(star + " ");
+
+//    }
+//    Console.WriteLine();
+//}
+//
+//
+//
+//int[] arr = { 211, 9, -6, 12, 34, 56, 32, 5, 19 };
+//int[] arr2 = {  5, 19 ,9};
+//bool check2=false;
+//for (int i = 0; i < arr2.Length; i++)
+//{
+//    check2 = false;
+//    for(int j = 0; j < arr.Length; j++)
+//    {
+//        if (arr2[i] == arr[j])
+//        {
+//            check2 = true;
+//            break;
+//        }
+       
+//    }
+//}
+//Console.WriteLine(check2);
