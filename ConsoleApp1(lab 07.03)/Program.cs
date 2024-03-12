3664a39383815f048664b557fb994be52bfb45ad
salvar <salvarcafarov@gmail.com>
2024-03-12 10:41:08 +0400

    12.03

---
+++ ConsoleApp1(lab 07.03)/Program.cs
@@ -0,0 +1,77 @@
+﻿//int[] arr = { 1, 2, 3, 4, 2 };
+//bool check = false;
+//int numb=0 ;
+//int count;
+//foreach (var item in arr)
+//{
+//count=0;
+//    foreach (var item2 in arr)
+//    {
+//        if (item2 == item)
+//        {
+//            numb = item2;
+//            count++;
+//        }
+
+//    }
+//   if(count ==1) {
+//        Console.WriteLine(numb);
+//    }
+//}
+
+
+
+//int[] arr3 = { 1, 2, 3 };
+//int[] arr4 = new int[arr3.Length];
+//for (int i = 0; i < arr3.Length; i++)
+//{
+//    arr4[i]= arr3[i];
+//}
+
+//foreach (var item in arr4)
+//{
+//    Console.WriteLine(item);
+//}
+
+
+int[] arr = { 1, 2,5,5,5, 3, 4, 2,1 };
+int[] arr2 = new int[arr.Length];
+bool check = false;
+int numb = 0;
+int count;
+int counter = 0;
+foreach (var item in arr)
+{
+    count = 0;
+    foreach (var item2 in arr)
+    {
+        foreach (var item3 in arr2)
+        {
+            if (item == item3)
+            {
+                check = true;
+            }
+        }
+        if (!check)
+        {
+            if (item2 == item)
+            {
+
+                numb = item2;
+                count++;
+
+            }
+        }
+
+
+    }
+
+    if (!check)
+    {
+        Console.WriteLine(numb + " " + count);
+    }
+    arr2[counter]=item;
+    counter++;
+    check = false;
+}
+
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,104 @@
+﻿//task1
+////int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
+//int count = 0;
+//foreach (var item in arr)
+//{
+//    if (item % 2 == 1)
+//    {
+//        count++;
+//    }
+//};
+
+//Console.WriteLine(count);
+
+//task2
+
+//int[] arr2 = { 3, 8, 2, 10, 5 };
+//int min = 100;
+//int min = arr2[0];
+//int max = arr2[0];
+//foreach (int i in arr2)
+//{
+//    if (i > max)
+//    {
+//        max = i;
+//    }
+//    if (i < min)
+//    {
+//        min = i;
+//    }
+//};
+
+//sum = min + max;
+//Console.WriteLine(sum);
+
+//task3
+
+//int[] arr2 = { 3, 8, 2, 10, 5 };
+//int min = arr2[0];
+//int max = arr2[0];
+//int minus = 0;
+//foreach (int i in arr2)
+//{
+//    if (i > max)
+//    {
+//        max = i;
+//    }
+//    if (i < min)
+//    {
+//        min = i;
+//    }
+//};
+
+//minus =  max-min;
+//Console.WriteLine(minus);
+
+
+
+//task5
+
+//int[] arr4 = { 1, 2, 3, 4, 5 };
+//int even = 0;
+
+//int odd = 0;
+//foreach (var item in arr4)
+//{
+//    if (item % 2 == 1)
+//    {
+//        odd+=item;
+//    }
+
+//    else if (even % 2 == 0)
+//    {
+//        even+=item;
+//    }
+//};
+//Console.WriteLine(odd);
+//Console.WriteLine(even);
+
+
+
+//task4
+
+int[] arr9 = { 1,9, 3, 8, 5, 4 };
+bool check = true;
+while (check)
+{
+    check = false;
+    Console.WriteLine("salam");
+    for(int i = 0; i < arr9.Length-1; i++)
+    {
+    int temp = 0;
+        if (arr9[i] > arr9[i + 1])
+        {
+            temp = arr9[i];
+            arr9[i] = arr9[i + 1];
+            arr9[i + 1] = temp;
+            check = true;
+        }
+    }
+}
+foreach (var item in arr9)
+{
+    Console.WriteLine(item);
+}
